4294a5b27727afa0491a19f8f89ae144a7cf55c6
andersonmcook <andersonmcook@gmail.com>
2016-02-03 22:08:51 -0600

    styling, jquery

--- public/javascripts/use-jquery.js
+++ public/javascripts/use-jquery.js
@@ -1,8 +1,10 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 
+  $(".logo").fadeIn(2000, "swing");
+
   $("h1").hover(function () {
-    $("#tag").fadeToggle("slow", "linear");
+    $("#tag").fadeToggle(1000, "swing");
   });
 
 });
