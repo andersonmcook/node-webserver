e3c7aa90b6b8e92291160238d370f0bd1a32229d
andersonmcook <andersonmcook@gmail.com>
2016-02-15 12:20:23 -0600

    renamed variable

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const mongoose = require('mongoose');
 const MONGODB_HOST = process.env.MONGODB_HOST || 'localhost';
 const MONGODB_PORT = process.env.MONGODB_PORT || 27017;
 const MONGODB_USER = process.env.MONGODB_USER || '';
-const MONGODB_PASS = process.env.MONGOD_PASS || '';
+const MONGODB_PASS = process.env.MONGODB_PASS || '';
 const MONGODB_DB = 'node-webserver';
 const MONGODB_URL_PREFIX = MONGODB_USER ? `${MONGODB_USER}:${MONGODB_PASS}@` : '';
 
